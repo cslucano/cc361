191a7edfc1f98a577dfdb28fe168f0eb987603ca
Carlos <cslucano@gmail.com>
2012-05-04 22:48:51 -0500

    Modificación el la impresión del buffer

--- pc.c
+++ pc.c
@@ -11,12 +11,44 @@ typedef struct {
   int BUFFER[BUFF_TAM];
   int in;
   int out;
+  int contador;
   sem_t lleno;
   sem_t vacio;
   sem_t mutex;
 } std_entorno;
 
 std_entorno shared;
+
+
+void imprimir_buffer()
+{
+    int j;
+    for(j=0;j<BUFF_TAM;j++)
+    {
+      if(shared.in>shared.out)
+      {
+        if(j<shared.out) printf("_");
+        else if(j>=shared.out && j<shared.in) printf("*");
+        else if(j>=shared.in) printf("_");
+      }
+      if(shared.in<shared.out)
+      {
+        if(j<shared.in) printf("*");
+        else if(j>=shared.in && j<shared.out) printf("_");
+        else if(j>=shared.out) printf("*");
+      }
+      if(shared.in==shared.out)
+      {
+        if(shared.contador==0) printf("_");
+        else printf("*");
+      }
+    }
+    printf(" %d", shared.contador);
+    if(shared.contador == 1) printf(" elemento");
+    else printf(" elementos\n", shared.contador);
+    printf("\n");
+    return;
+}
 
 void *Productor(void *arg){
   int item;
@@ -28,19 +60,9 @@ void *Productor(void *arg){
     sem_wait(&shared.mutex);
     shared.BUFFER[shared.in] = item;
     shared.in = (shared.in+1)%BUFF_TAM;
-    printf("Productor item(%d)",item);
-    int j;
-    for(j=0;j<BUFF_TAM;j++)
-    {
-      if(shared.in>shared.out)
-      {
-        if(j<shared.out) printf("_");
-        else if(j>shared.out && j<shared.in) printf("*");
-        else if(j>=shared.in) printf("_");
-      }
-    }
-    printf("\n");
-   
+    shared.contador++;
+    printf("Productor item(%d)\t\t\t\t",item);
+    imprimir_buffer();   
     sem_post(&shared.mutex);
     sem_post(&shared.lleno);     
 
@@ -58,13 +80,16 @@ void *Consumidor(void *arg){
     sem_wait(&shared.mutex);
     item = shared.BUFFER[shared.out];
     shared.out = (shared.out+BUFF_TAM+1)%BUFF_TAM;
-    printf("-------------->Consumidor item(%d)\n",item);
+    shared.contador--;
+    printf("\t\t\tConsumidor item(%d)\t",item);
+    imprimir_buffer();
     sem_post(&shared.mutex);
     sem_post(&shared.vacio);
     if(rand()%2)sleep(1);
   }
   return NULL;
 }
+
 
 int main()
 {
@@ -73,6 +98,7 @@ int main()
 
   shared.in = 0; 
   shared.out = 0; 
+  shared.contador = 0;
   sem_init(&shared.lleno, 0, 0);
   sem_init(&shared.vacio, 0, 5);
   sem_init(&shared.mutex, 0, 1);
